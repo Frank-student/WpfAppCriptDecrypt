9c5eb1b5408cec8e33ffc76e3047b8045f5a2d57
Frank-student <suba.francisc@student.uoradea.ro>
2024-11-12 13:41:09 +0200

    savin progress

---
+++ WpfAppCriptDecrypt/Crypting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppCriptDecrypt
+{
+    public class Cripting
+    {
+        public static string Criptare(string text)
+        {
+            string textCriptat = "";
+            char randomChar;
+            //initializare generator numere aleatoare
+            Random rand = new Random();
+            //cripteaza fiecare caracter din textul initial
+            for (int i = 0; i <= (text.Length - 1); i++)
+            {
+                //genereaza un caracter aleator
+                randomChar = (char)(rand.Next(128));
+                //XOR intre caracterul curent si caracterul generat aleator
+                textCriptat += ((char)(text[i] ^ randomChar)).ToString();
+                //adaugam caracterul generat aleator la mesajul criptat 
+                //cu XOR 128 - i pentru a-l avea la decriptare
+                textCriptat += ((char)(randomChar ^ (128 - i))).ToString();
+            }
+            return textCriptat;
+        }
+
+    }
+}
