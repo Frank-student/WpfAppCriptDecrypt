f6730bcec69aab4136313d079a30577b92dd5d9e
Frank-student <suba.francisc@student.uoradea.ro>
2024-11-26 13:41:03 +0200

    Lab 01

--- WpfAppCriptDecrypt/MainWindow.xaml.cs
+++ WpfAppCriptDecrypt/MainWindow.xaml.cs
@@ -35,8 +35,6 @@ namespace WpfAppCriptDecrypt
             //se va declansa atunci cand BackgroundWorker isi termina executia
             bkworker.RunWorkerCompleted += bkworker_RunWorkerCompleted;
             lbl_text_afis.Content = "Hello BackgroudWorker!";
-            string[] args =[lbl_text_afis.Content];   
-            Culori(args );
         }
         //metoda (procesarea) pe care o executa BackgroundWorker
         void bkworker_DoWork(object? sender, DoWorkEventArgs e)
@@ -63,7 +61,7 @@ namespace WpfAppCriptDecrypt
         void bkworker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
         {
             //daca a fost Cancel
-            if (e.Cancelled) lbl_text_afis.Content += " Cancel BackgoroudWorker!";
+            if (e.Cancelled) lbl_text_afis.Content += " Canceled!";
             //daca a aparut o exceptie
             else if (e.Error != null)
                 lbl_text_afis.Content = "Exceptie BackgoroudWorker : " + e.Error.ToString();
@@ -77,67 +75,81 @@ namespace WpfAppCriptDecrypt
             this.progres.Value = e.ProgressPercentage;
             lbl_text_afis.Content = "Am ajuns la " + e.ProgressPercentage + "%";
         }
-        //metoda handler buton Start
+        //metoda handler buton Start Cripting
         private void btn_Start_Click(object sender, RoutedEventArgs e)
         {
             bkworker.RunWorkerAsync(100);
+            string[] args = { "one", "two" };
+            Procesare(args);
         }
         //metoda handler buton Cancel
         private void btn_Cancel_Click(object sender, RoutedEventArgs e)
         {
+            text_crypted.Text = string.Empty; 
+            text_decrypted.Text = string.Empty;
             if (bkworker.IsBusy) bkworker.CancelAsync();
         }
-        static void Culori(string[] args)
+        public void Procesare(string[] args)
         {
-            Task<String> t1 = new Task<String>(() => AfisNumere());
-            Task<String> t2 = new Task<String>(() => AfisCuvinte());
-            Task<String> t3 = new Task<String>(() => AfisCulori());
+            Task<String> t1 = new Task<String>(() => Cripting.Criptare(text_originall.Text));
+            //Task<String> t2 = new Task<String>(() => AfisCuvinte());
+            //Task<String> t3 = new Task<String>(() => AfisCulori());
             //pornire task-uri utilizand Start
             t1.Start();
-            t2.Start();
-            t3.Start();
+            //t2.Start();
+            //t3.Start();
+
+            ////alternativa cu Parallel.Invoke
+            //Parallel.Invoke
+            // (
+            // new Action(),
+            // new Action(),
+            // new Action()
+            // );
+
+
             //afisare valori returnate de task - proprietatea Result
             //.NET blocheaza thread-ul main pentru a astepta finalizarea task-urilor
             System.Diagnostics.Debug.WriteLine(t1.Result);
-            System.Diagnostics.Debug.WriteLine(t2.Result);
-            System.Diagnostics.Debug.WriteLine(t3.Result);
+            //System.Diagnostics.Debug.WriteLine(t2.Result);
+            //System.Diagnostics.Debug.WriteLine(t3.Result);
             Console.ReadLine();
         }
-        static String AfisNumere()
-        {
-            Thread.CurrentThread.Name = "Thread-ul 1";
-            for (int i = 0; i < 5; i++)
-            {
-                System.Diagnostics.Debug.WriteLine(String.Format("Numele thread-ului {0} numarul curent {1}", Thread.CurrentThread.Name, i));
-                Thread.Sleep(1000);
-            }
-            return String.Format("Task-ul cu numele {0} si-a finalizat executia!",
-            Thread.CurrentThread.Name);
-        }
-        static String AfisCulori()
-        {
-            Thread.CurrentThread.Name = "Thread-ul 3";
-            String[] culori = { "rosu", "galben", "verde", "albastru", "portocaliu" };
-            foreach (String c in culori)
-            {
-                System.Diagnostics.Debug.WriteLine(String.Format("Numele thread-ului {0} numarul curent {1}", Thread.CurrentThread.Name, c));
-                Thread.Sleep(1000);
-            }
-            return String.Format("Task-ul cu numele {0} si-a finalizat executia!",
-            Thread.CurrentThread.Name);
-        }
-        static String AfisCuvinte()
-        {
-            Thread.CurrentThread.Name = "Thread-ul 2";
-            String text = "Exemplu de sir utilizat pentru crearea taskurilor concurente";
-            String[] cuvinte = text.Split(' ');
-            foreach (String s in cuvinte)
-            {
-                System.Diagnostics.Debug.WriteLine(String.Format("Numele thread-ului {0} numarul curent {1}", Thread.CurrentThread.Name, s));
-                Thread.Sleep(1000);
-            }
-            return String.Format("Task-ul cu numele {0} si-a finalizat executia!",
-            Thread.CurrentThread.Name);
-        }
+        //static String AfisNumere()
+        //{
+        //    Thread.CurrentThread.Name = "Thread-ul 1";
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine(String.Format("Numele thread-ului {0} numarul curent {1}", Thread.CurrentThread.Name, i));
+        //        Thread.Sleep(1000);
+        //    }
+        //    return String.Format("Task-ul cu numele {0} si-a finalizat executia!",
+        //    Thread.CurrentThread.Name);
+        //}
+        //static String AfisCulori()
+        //{
+        //    Thread.CurrentThread.Name = "Thread-ul 3";
+        //    String[] culori = { "rosu", "galben", "verde", "albastru", "portocaliu" };
+        //    foreach (String c in culori)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine(String.Format("Numele thread-ului {0} numarul curent {1}", Thread.CurrentThread.Name, c));
+        //        Thread.Sleep(1000);
+        //    }
+        //    return String.Format("Task-ul cu numele {0} si-a finalizat executia!",
+        //    Thread.CurrentThread.Name);
+        //}
+        //static String AfisCuvinte()
+        //{
+        //    Thread.CurrentThread.Name = "Thread-ul 2";
+        //    String text = "Exemplu de sir utilizat pentru crearea taskurilor concurente";
+        //    String[] cuvinte = text.Split(' ');
+        //    foreach (String s in cuvinte)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine(String.Format("Numele thread-ului {0} numarul curent {1}", Thread.CurrentThread.Name, s));
+        //        Thread.Sleep(1000);
+        //    }
+        //    return String.Format("Task-ul cu numele {0} si-a finalizat executia!",
+        //    Thread.CurrentThread.Name);
+        //}
     }
 }
