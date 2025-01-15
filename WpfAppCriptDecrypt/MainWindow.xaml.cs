b6aa32dedcc3306a02382ca9e4430117b76884b3
Francis <65733799+MaxiFrancis@users.noreply.github.com>
2025-01-15 21:37:59 +0200

    finishing

--- WpfAppCriptDecrypt/MainWindow.xaml.cs
+++ WpfAppCriptDecrypt/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
@@ -13,147 +12,20 @@ namespace WpfAppCriptDecrypt
 {
     public partial class MainWindow : Window
     {
-        BackgroundWorker bkworkerEncrypt = new BackgroundWorker();
-        BackgroundWorker bkworkerDecrypt = new BackgroundWorker();
         private bool isPaused = false;
         private CancellationTokenSource? cts;
 
         public MainWindow()
         {
             InitializeComponent();
-            bkworkerEncrypt.WorkerReportsProgress = true;
-            bkworkerEncrypt.WorkerSupportsCancellation = true;
-            bkworkerEncrypt.DoWork += bkworkerEncrypt_DoWork;
-            bkworkerEncrypt.ProgressChanged += bkworker_ProgressChanged;
-            bkworkerEncrypt.RunWorkerCompleted += bkworker_RunWorkerCompleted;
-
-            bkworkerDecrypt.WorkerReportsProgress = true;
-            bkworkerDecrypt.WorkerSupportsCancellation = true;
-            bkworkerDecrypt.DoWork += bkworkerDecrypt_DoWork;
-            bkworkerDecrypt.ProgressChanged += bkworker_ProgressChanged;
-            bkworkerDecrypt.RunWorkerCompleted += bkworker_RunWorkerCompleted;
-
-            lbl_text_afis.Content = "Hello BackgroudWorker!";
-        }
-
-        void bkworkerEncrypt_DoWork(object? sender, DoWorkEventArgs e)
-        {
-            string text = (string)e.Argument;
-            string textCriptat = "";
-            string key = "";
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (bkworkerEncrypt.CancellationPending)
-                {
-                    e.Cancel = true;
-                    return;
-                }
-
-                while (isPaused)
-                {
-                    Thread.Sleep(100);
-                }
-
-                var (encryptedChar, keyChar) = Cripting.CriptareChar(text[i], i);
-                textCriptat += encryptedChar;
-                key += keyChar;
-
-                int progressPercentage = (i + 1) * 100 / text.Length;
-                bkworkerEncrypt.ReportProgress(progressPercentage, new Tuple<string, string>(textCriptat, key));
-
-                Thread.Sleep(100);
-            }
-            e.Result = new Tuple<string, string>(textCriptat, key);
-        }
-
-        void bkworkerDecrypt_DoWork(object? sender, DoWorkEventArgs e)
-        {
-            var data = (Tuple<string, string>)e.Argument;
-            string text = data.Item1;
-            string key = data.Item2;
-            string textDecriptat = "";
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (bkworkerDecrypt.CancellationPending)
-                {
-                    e.Cancel = true;
-                    return;
-                }
-
-                while (isPaused)
-                {
-                    Thread.Sleep(100);
-                }
-
-                char decryptedChar = Cripting.DecriptareChar(text[i], key[i], i);
-                textDecriptat += decryptedChar;
-
-                int progressPercentage = (i + 1) * 100 / text.Length;
-                bkworkerDecrypt.ReportProgress(progressPercentage, textDecriptat);
-
-                Thread.Sleep(100);
-            }
-            e.Result = textDecriptat;
-        }
-
-        void bkworker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
-        {
-            if (e.Cancelled)
-            {
-                lbl_text_afis.Content += " Canceled!";
-                if (sender == bkworkerEncrypt)
-                {
-                    text_crypted.Text += " (Invalid)";
-                }
-                else if (sender == bkworkerDecrypt)
-                {
-                    text_decrypted.Text += " (Invalid)";
-                }
-            }
-            else if (e.Error != null)
-            {
-                lbl_text_afis.Content = "Exceptie BackgoroudWorker : " + e.Error.ToString();
-            }
-            else
-            {
-                if (sender == bkworkerEncrypt)
-                {
-                    var result = (Tuple<string, string>)e.Result;
-                    text_crypted.Text = result.Item1;
-                    text_decrypted.Tag = result.Item2; 
-                    lbl_text_afis.Content = "Encryption Finalized!";
-                }
-                else if (sender == bkworkerDecrypt)
-                {
-                    text_decrypted.Text = (string)e.Result;
-                    lbl_text_afis.Content = "Decryption Finalized!";
-                }
-            }
-        }
-
-        void bkworker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
-        {
-            this.progres.Value = e.ProgressPercentage;
-            if (e.UserState is Tuple<string, string> encryptState)
-            {
-                text_crypted.Text = encryptState.Item1;
-            }
-            else if (e.UserState is string decryptState)
-            {
-                text_decrypted.Text = decryptState;
-            }
-            lbl_text_afis.Content = "Am ajuns la " + e.ProgressPercentage + "%";
+            lbl_text_afis.Content = "Hello BackgroundWorker!";
         }
 
         private void btn_Start_Click(object sender, RoutedEventArgs e)
         {
             isPaused = false;
             string processingType = ((ComboBoxItem)comboBoxProcessingType.SelectedItem).Content.ToString();
-            if (processingType == "BackgroundWorker")
-            {
-                bkworkerEncrypt.RunWorkerAsync(text_originall.Text);
-            }
-            else if (processingType == "Task")
+            if (processingType == "Task")
             {
                 cts = new CancellationTokenSource();
                 var token = cts.Token;
@@ -287,7 +159,7 @@ namespace WpfAppCriptDecrypt
                     Dispatcher.Invoke(() =>
                     {
                         text_crypted.Text = new string(textCriptat.ToArray());
-                        text_decrypted.Tag = new string(key.ToArray()); 
+                        text_decrypted.Tag = new string(key.ToArray());
                         lbl_text_afis.Content = "Encryption Finalized!";
                     });
                 }, token);
@@ -298,12 +170,7 @@ namespace WpfAppCriptDecrypt
         {
             isPaused = false;
             string processingType = ((ComboBoxItem)comboBoxProcessingType.SelectedItem).Content.ToString();
-            if (processingType == "BackgroundWorker")
-            {
-                var data = new Tuple<string, string>(text_crypted.Text, (string)text_decrypted.Tag);
-                bkworkerDecrypt.RunWorkerAsync(data);
-            }
-            else if (processingType == "Task")
+            if (processingType == "Task")
             {
                 cts = new CancellationTokenSource();
                 var token = cts.Token;
@@ -445,8 +312,6 @@ namespace WpfAppCriptDecrypt
 
         private void btn_Cancel_Click(object sender, RoutedEventArgs e)
         {
-            if (bkworkerEncrypt.IsBusy) bkworkerEncrypt.CancelAsync();
-            if (bkworkerDecrypt.IsBusy) bkworkerDecrypt.CancelAsync();
             cts?.Cancel();
             isPaused = false;
         }
